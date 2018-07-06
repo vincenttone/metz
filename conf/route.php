42779fa4afa7cd0f997fa70d617335d4ee2faf11
vincenttone <tongwensong@gmail.com>
2018-07-06 06:56:01 +0000

    common route

---
+++ conf/route.php
@@ -0,0 +1,27 @@
+<?php
+namespace Metz\configure;
+
+use Metz\app\metz\route\Restful as R;
+use Metz\app\metz\route\Common as C;
+
+class Route
+{
+    static function configure()
+    {
+        return [
+            /*
+            new R('/',                \Metz\app\test\A::class, 'z'),
+            new R('/call',            \Metz\app\test\A::class, 'x'),
+            new R('/call/some/where', \Metz\app\test\A::class, 'y'),
+            new R('/call/this',       \Metz\app\test\A::class),
+            new R('wall/li',          '\\Metz\\app\\test'),
+            new R('metz/app/test/a'),
+            new C('/', \Metz\app\test\A::class),
+            new C('metz/app/test'),
+            new C('/c/play',           '\\Metz\\app\\test'),
+            new C('/c/play/m',          \Metz\app\test\A::class, 'z'),
+            */
+            new C('/',           '\\Metz\\app\\metz'),
+        ];
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once(dirname(dirname(__FILE__)) . '/init.php');
-Metz\app\metz\Router::get_instance()->load_configure(Metz\configure\Route::configure());
 Metz\sys\Router::get_instance()
-    ->register_router([Metz\app\metz\Router::get_instance(), 'route'])
+    ->register_router(
+        [
+            Metz\app\metz\Router::get_instance()->load_configure(Metz\configure\Route::configure()),
+            'route'
+        ]
+    )
     ->dispatch();
